94b008ddbf5d47eb8181c7237d22564f7d915c82
Baird <bairdilyon@gmail.com>
2026-05-26 20:45:25 -0700

    Refactor drawing logic and update mouse handling

--- sketch.js
+++ sketch.js
@@ -2,21 +2,21 @@ var setup = function(){
     createCanvas(600,600);
 }
 var verts = [];
+mouseReleased = function(){
+   	verts.push({x:mouseX,y:mouseY});
+};
 draw = function(){
-    strokeJoin(ROUND);
+    strokeJoin(ROUND);b
     background(0);
-    noStroke();
-    fill(255,0,0);
+    stroke(255,0,0);
+    noFill();
     beginShape();
     	for(var i = 0; i < verts.length; i++){
             var v = verts[i];
         	vertex(v.x,v.y);
         }
-        if(mouseIsPressed){
-            verts.push({x:mouseX,y:mouseY});
-        }
-    endShape(CLOSE);
+    endShape();
     if(keyIsPressed && keyCode === 32 && verts.length > 0){
-        verts.splice(verts.length-1,1);
+        verts.splice(verts.length,1);
     }
 };
