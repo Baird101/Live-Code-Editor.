4a3bb70af39af0908cac321dcf1009c905e110ea
Baird <bairdilyon@gmail.com>
2026-05-27 09:24:02 -0700

    Refactor sketch.js to add graphics and rotation

--- sketch.js
+++ sketch.js
@@ -1,24 +1,41 @@
 var setup = function(){
+    rectMode(CENTER);
     createCanvas(600,600);
+    g = createGraphics(600,600);
+    g.noStroke();
+    g.rectMode(CENTER);
+    g.fill(70,70,70);
+    g.rect(300,300,300,300);
+    fill(0,0,0);
+	rect(300,300,200,200);
+	ground = get(0,0,600,600);
 }
 var verts = [];
-mouseReleased = function(){
-   	verts.push({x:mouseX,y:mouseY});
-};
-keyReleased = function(){
-    if(keyCode === 32 && verts.length > 0){
-        verts.splice(verts.length-1,1);
-    }
-};
 draw = function(){
     strokeJoin(ROUND);
-    background(0);
-    stroke(255,0,0);
-    noFill();
+    imageMode(CENTER);
+    
+    background(255,0,0);
+    pushMatrix();
+    	translate(300,300);
+    	rotate(frameCount/100);
+    	fill(30,30,30);
+		rect(0,0,400,400);
+    	image(g,0,0);
+    	image(ground,0,0);
+    popMatrix();
+    noStroke();
+    fill(255,0,0);
     beginShape();
     	for(var i = 0; i < verts.length; i++){
             var v = verts[i];
         	vertex(v.x,v.y);
         }
-    endShape();
+        if(mouseIsPressed){
+            verts.push({x:mouseX,y:mouseY});
+        }
+    endShape(CLOSE);
+    if(keyIsPressed && keyCode === 32 && verts.length > 0){
+        verts.splice(verts.length-1,1);
+    }
 };
